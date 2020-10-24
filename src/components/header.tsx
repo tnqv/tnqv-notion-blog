45e06698cc1fdd4ae95bdfd2e4160329be03d4e8
tnqv <tnqv1021997@gmail.com>
2020-10-24 14:10:45 +0700

    Fix header file

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,4 @@
-import Link from 'next/link'
 import Head from 'next/head'
-import ExtLink from './ext-link'
 import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
@@ -18,12 +16,11 @@ export default ({ titlePre = '' }) => {
         <title>{titlePre ? `${titlePre} |` : ''} TNQV Blog</title>
         <meta
           name="description"
-          content="An example Next.js site using Notion for the blog"
+          content="An Next.js site using Notion for the blog"
         />
         <meta name="og:title" content="TNQV Blog" />
         <meta name="twitter:site" content="@tnqvsys" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={ogImageUrl} />
       </Head>
       <a onClick={handleClick}>Back to home</a>
     </header>
